e3c639cc16abb2b748792b8b9fd5a4ce1365b13b
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-12-19 01:22:54 +0100

    main parte 2

--- pr2/main.py
+++ pr2/main.py
@@ -1,14 +1,14 @@
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 import gymnasium as gym
 import pygame # hace falta para pintar el tablero graficamente en una ventanita
 from Qlearning import *
 
-random.seed(10)  # semilla de numeros pseudo-aleatorios
 
-env = gym.make("FrozenLake-v1",map_name="4x4",is_slippery=False, render_mode="none") # reemplazar none por human para ver el muneco
 
+env = gym.make("FrozenLake-v1",map_name="4x4",is_slippery=False, render_mode="none",desc = generate_random_map(size=2)) # reemplazar none por human para ver el muneco
 
 # Apartado e)
-qle = Qlearning(env.observation_space.n, 4, 0.7, 0.95, 15000, env, 99,0.3)
-
+qle = Qlearning(env.observation_space.n, 4, 0.7, 0.95, 10, env, 99,0.9)
+  # semilla de numeros pseudo-aleatorios
 '''Realizamos el entrenamiento con los parámetros indicados, y mostramos la matriz obtenida.'''
 print(qle.entrenamiento())
